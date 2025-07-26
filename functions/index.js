454f50733ddfbd86bf847cc98de55083662bb118
selami538 <erdaltayfun37@gmail.com>
2025-07-26 20:21:51 +0300

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -16,7 +16,7 @@ export async function onRequest(context) {
     return String(parseInt(match) + 1);
   });
 
-  const apiUrl = "https://panelcorespor.corepanel.pro/api/verirepo.php";
+  const apiUrl = "https://corepanel.corepanel.pro/api/verirepo.php";
 
   let title = "";
   let description = "";
